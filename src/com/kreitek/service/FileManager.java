f2f3a3bbdac0bd4fd7c2eb2e074c1c3786343d57
mdmmr97 <mdmmr97@hotmail.com>
2023-03-30 08:32:34 +0200

    Eliminado el metodo duplicado en la clase FileManager

--- src/com/kreitek/service/FileManager.java
+++ src/com/kreitek/service/FileManager.java
@@ -9,26 +9,6 @@ public class FileManager {
         return totalSize = fileSystemItem.getSize();
     }
 
-    /*
-
-    Este metodo esta duplicado, y aparte no cumpliria con el principio de inversion de dependencias
-    porqu
-    public static int calculateSize(List<FileSystemItem> files) {
-        int totalSize = 0;
-
-        for(FileSystemItem item : files) {
-            if (item instanceof File) {
-                totalSize += item.getSize();
-            } else if (item instanceof Directory) {
-                totalSize += calculateSize(item.listFiles());
-            }
-        }
-
-        return totalSize;
-    }
-
-     */
-
     // Aquí habría otros métodos para gestionar ficheros y directorios:
     // Crear ficheros, mover ficheros, eliminar ficheros, etc.
 }
